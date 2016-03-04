067c73e5c1a05aeba73900fb41de9884dde415c7
Dmitry Simonenko <pmwkaa@gmail.com>
2016-03-04 17:27:37 +0300

    sophia: use gettimeofday() on Mac OS

--- sophia/std/ss_time.c
+++ sophia/std/ss_time.c
@@ -19,7 +19,13 @@ void ss_sleep(uint64_t ns)
 
 uint64_t ss_utime(void)
 {
+#if defined(__APPLE__)
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	return t.tv_sec * 1000000ULL + t.tv_usec;
+#else
 	struct timespec t;
 	clock_gettime(CLOCK_MONOTONIC, &t);
 	return t.tv_sec * 1000000ULL + t.tv_nsec / 1000;
+#endif
 }
